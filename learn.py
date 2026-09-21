7b50bd74c23f52f801b79a760e83e518295e7b2e
jood Al-muflihi <jood2005almuflihi@gmail.com>
2026-09-21 04:18:23 +0300

    Add files via upload

---
+++ learn.py
@@ -0,0 +1,19 @@
+print(" | Welcome to Calculator App |")
+
+First_number = float(input(" first number: "))
+Second_number = float(input( "second number: "))
+
+Additing = First_number + Second_number
+subtract = First_number - Second_number
+Multiply = First_number * Second_number
+Divide = First_number / Second_number
+modulus = First_number % Second_number
+
+
+print("____________")
+
+print("Additing: ", Additing)
+print("Subtract: ", subtract)
+print("Multiply: ", Multiply)
+print("Divide: ", Divide)
+print("Modulus: ", modulus)
