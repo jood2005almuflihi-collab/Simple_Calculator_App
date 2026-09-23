c2d130b768b22859147fd97568bec44250117aa2
jood Al-muflihi <jood2005almuflihi@gmail.com>
2026-09-23 10:58:46 +0300

    Refactor calculator app for clarity and consistency
    Refactored calculator app to use consistent variable names and improved formatting.

--- learn.py
+++ learn.py
@@ -1,19 +1,14 @@
-print(" | Welcome to Calculator App |")
+print(" | Welcome to Calculator App | \n") #\n for new line
 
-First_number = float(input(" first number: "))
-Second_number = float(input( "second number: "))
+num1 = float(input(" first number: "))
+num2 = float(input( "second number: "))
 
-Additing = First_number + Second_number
-subtract = First_number - Second_number
-Multiply = First_number * Second_number
-Divide = First_number / Second_number
-modulus = First_number % Second_number
 
 
 print("____________")
 
-print("Additing: ", Additing)
-print("Subtract: ", subtract)
-print("Multiply: ", Multiply)
-print("Divide: ", Divide)
-print("Modulus: ", modulus)
+print("Additing: ", num1 + num2)
+print("Subtract: ", num1 - num2)
+print("Multiply: ", num1 * num2)
+print("Divide: ", num1 / num2)
+print("Modulus: ", num1 % num2)
